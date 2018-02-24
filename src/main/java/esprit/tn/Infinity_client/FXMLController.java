979e10afb32b0dbdb0b81164e82f62ddb2aba14f
Mdaghi <seifeddine.m'daghi@esprit.tn>
2018-02-24 14:40:25 +0100

    Maven + JavaFx + JNDI Commit 1 : Infinity_client Title : Project maven with JavaFx plugins configurate -In this Project there is a simple exemple that allow you how to use  an EJB from Infinity_server (AddressService) Using a JavaFx
    1) Pom.xml configuration with javaFx (valid)
    2) Using JNDI service to use AddressService (valid)
    3) Inserting in dataBase using JavaFx ButtonAction (Valid)
    4) Project can also be used in Netbeans without an Error (Valid)

---
+++ src/main/java/esprit/tn/Infinity_client/FXMLController.java
@@ -0,0 +1,43 @@
+package esprit.tn.Infinity_client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import tn.esprit.infinity_server.interfaces.AddressRemote;
+import tn.esprit.infinity_server.persistence.Address;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws NamingException {
+    	String jndiName="infinity_server-ear/infinity_server-ejb/ServiceAddress!tn.esprit.infinity_server.interfaces.AddressRemote";
+		Context context=new InitialContext();
+		AddressRemote proxy=(AddressRemote)context.lookup(jndiName);
+		//
+		Address address = new Address();
+		address.setCity("Tunis");
+		address.setCountry("Tunisia");
+		address.setNumber(9);
+		address.setPostalCode("2048");
+		address.setStreet("Habib bourguiba");
+		//
+		proxy.CreateAddress(address);
+        label.setText("Insert the first Address in DB with success ");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
